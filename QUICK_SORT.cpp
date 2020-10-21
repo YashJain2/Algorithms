9beeb5535bc3a52511637e60fc719279638d159d
snehaa1989 <61073116+snehaa1989@users.noreply.github.com>
2020-10-21 11:39:30 +0530

    QUICKSORT
    Input Format
    A single integer, N, denoting the number of elements in array. Next line contains N integers, denoting the elements of array.
    
    Constraints
    1<=N<=2*10^5
    |Ai|<=10^9
    
    Output Format
    Print in a single line, N spaced integers, denoting the elements of array A in sorted order.
    
    Sample Input
    5
    3 6 4 1 2
    Sample Output
    1 2 3 4 6

---
+++ QUICK_SORT.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+int partition(int a[],long long int s,long long int e)
+{
+long long int i=s-1;
+long long int j=s;
+long long int pivot=a[e];
+for(j=s;j<=e-1;j++)
+{
+    if(a[j]<=pivot)
+    {
+        i=i+1;
+        swap(a[i],a[j]);
+    }
+}
+swap(a[i+1],a[e]);
+return i+1;
+}
+void quick_sort(int a[],long long int s,long long int e){
+    if(s>=e)
+    {
+        return;
+    }
+    //rec case
+    long long int p=partition(a,s,e);
+    quick_sort(a,s,p-1);
+    quick_sort(a,p+1,e);
+}
+
+int main() {
+    //quicksort algorithm
+    //divide and conquer strategy
+    //nlogn time for average case
+    //n^2 for worst case,inplace sort algorithm, wont take extra space
+	long long int n;
+   
+	cin>>n;
+	 int arr[n];
+    for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];
+	}
+    quick_sort(arr,0,n-1);
+    for(int i=0;i<n;i++)
+    {
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
