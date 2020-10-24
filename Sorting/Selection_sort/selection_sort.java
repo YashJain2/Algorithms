ea4d13f1ae494084feff19f3fe09503ad054fbf2
Yash-Vijayvargiya <66905654+Yash-Vijayvargiya@users.noreply.github.com>
2020-10-24 18:51:36 +0530

    Update selection_sort.java

--- Sorting/Selection_sort/selection_sort.java
+++ Sorting/Selection_sort/selection_sort.java
@@ -1,11 +1,3 @@
-/******************************************************************************
-
-                            Online Java Compiler.
-                Code, Compile, Run and Debug java program online.
-Write your code in this editor and press "Run" button to execute it.
-
-*******************************************************************************/
-
 public class Main
 {
 	void selection_sort(int arr[])
